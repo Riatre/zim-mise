b15adf0f27accfad84b0ffa0647d8fa9afb30aec
Riatre Foo <foo@riat.re>
2023-07-27 05:06:47 +0800

    Shhhhh...

--- init.zsh
+++ init.zsh
@@ -15,6 +15,5 @@
   local compfile=$1/functions/_mise
   if [[ ! -e $compfile || $compfile -ot $command ]]; then
     $command complete --shell zsh >| $compfile
-    print -u2 -PR "* Detected a new version 'mise'. Regenerated completions."
   fi
 } ${0:h}
