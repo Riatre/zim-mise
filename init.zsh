Riatre/zim-rtx

Move completion files to along side the init.zsh
zgen likes this more.

init.zsh
@@ -21,7 +21,7 @@
   fi
 
   # generating completions
-  local compfile="$1/functions/_rtx"
+  local compfile="$1/_rtx"
   if [[ ! -e "$compfile" || "$compfile" -ot "$command" ]]; then
     "$command" complete --shell zsh >| "$compfile"
   fi

Riatre/zim-mise

init.zsh
@@ -21,7 +21,7 @@
   source "$activatefile"
 
   # generating completions
-  local compfile="$1/functions/_mise"
+  local compfile="$1/_mise"
   if [[ ! -e "$compfile" || "$compfile" -ot "$command" ]]; then
     "$command" complete --shell zsh >| "$compfile"
   fi