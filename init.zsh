Riatre/zim-rtx

Do not run hook-env during startup
It will be run in precmd anyways.

Also, quote everything.

init.zsh
@@ -1,19 +1,18 @@
 (( ${+commands[rtx]} )) && () {
-  local command=${commands[rtx]}
+  local command="${commands[rtx]}"
 
   # generating activation file
-  local activatefile=$1/rtx-activate.zsh
-  if [[ ! -e $activatefile || $activatefile -ot $command ]]; then
-    $command activate zsh >| $activatefile
-    zcompile -UR $activatefile
+  local activatefile="$1/rtx-activate.zsh"
+  if [[ ! -e "$activatefile" || "$activatefile" -ot "$command" ]]; then
+    "$command" activate zsh >| "$activatefile"
+    zcompile -UR "$activatefile"
   fi
 
-  source $activatefile
-  source <($command hook-env -s zsh)
+  source "$activatefile"
 
   # generating completions
-  local compfile=$1/functions/_rtx
-  if [[ ! -e $compfile || $compfile -ot $command ]]; then
-    $command complete --shell zsh >| $compfile
+  local compfile="$1/functions/_rtx"
+  if [[ ! -e "$compfile" || "$compfile" -ot "$command" ]]; then
+    "$command" complete --shell zsh >| "$compfile"
   fi
 } ${0:h}

Riatre/zim-mise

init.zsh
@@ -1,19 +1,18 @@
 (( ${+commands[mise]} )) && () {
-  local command=${commands[mise]}
+  local command="${commands[mise]}"
 
   # generating activation file
-  local activatefile=$1/mise-activate.zsh
-  if [[ ! -e $activatefile || $activatefile -ot $command ]]; then
-    $command activate zsh >| $activatefile
-    zcompile -UR $activatefile
+  local activatefile="$1/mise-activate.zsh"
+  if [[ ! -e "$activatefile" || "$activatefile" -ot "$command" ]]; then
+    "$command" activate zsh >| "$activatefile"
+    zcompile -UR "$activatefile"
   fi
 
-  source $activatefile
-  source <($command hook-env -s zsh)
+  source "$activatefile"
 
   # generating completions
-  local compfile=$1/functions/_mise
-  if [[ ! -e $compfile || $compfile -ot $command ]]; then
-    $command complete --shell zsh >| $compfile
+  local compfile="$1/functions/_mise"
+  if [[ ! -e "$compfile" || "$compfile" -ot "$command" ]]; then
+    "$command" complete --shell zsh >| "$compfile"
   fi
 } ${0:h}