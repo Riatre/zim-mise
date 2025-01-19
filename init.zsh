f331d53093cfcc034a0ba7826d3f6a28e5f6d9b5
Riatre Foo <foo@riat.re>
2025-01-20 02:24:08 +0800

    use activate --no-hook-env

--- init.zsh
+++ init.zsh
@@ -4,20 +4,28 @@
   # generating activation file
   local activatefile="$1/mise-activate.zsh"
   if [[ ! -e "$activatefile" || "$activatefile" -ot "$command" ]]; then
-    "$command" activate zsh >| "$activatefile"
+    "$command" activate --no-hook-env zsh >| "$activatefile"
     zcompile -UR "$activatefile"
   fi
 
   source "$activatefile"
 
-  if (( $+functions[_mise_hook] )); then
+  _mise_hook() {
+    eval "$(command mise hook-env -s zsh)";
+  }
+  typeset -ag precmd_functions;
+  if [[ -z "${precmd_functions[(r)_self_destruct_mise_hook]+1}" ]]; then
     function _self_destruct_mise_hook {
-        _mise_hook
-        # remove self from precmd
-        precmd_functions=(${(@)precmd_functions:#_self_destruct_mise_hook})
-        builtin unfunction _self_destruct_mise_hook
+      _mise_hook
+      # remove self from precmd
+      precmd_functions=(${(@)precmd_functions:#_self_destruct_mise_hook})
+      builtin unfunction _self_destruct_mise_hook
     }
     precmd_functions=( _self_destruct_mise_hook ${(@)precmd_functions:#_mise_hook} )
+  fi
+  typeset -ag chpwd_functions;
+  if [[ -z "${chpwd_functions[(r)_mise_hook]+1}" ]]; then
+    chpwd_functions=( _mise_hook ${chpwd_functions[@]} )
   fi
 
   # generating completions
